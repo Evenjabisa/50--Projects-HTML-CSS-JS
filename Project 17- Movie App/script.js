5ffd95d9e8cd182d988de1e0777d09dcf732a775
Adel <Adel@gmail.com>
2025-10-22 11:57:54 +0300

    Add Project 17- Córdoba Movies Website

---
+++ Project 17- Movie App/script.js
@@ -0,0 +1,79 @@
+const API_KEY = "179a49557e3cc9dfea8507893b672a84";
+const API_URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en`;
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+const main = document.getElementById("main");
+const form = document.getElementById("form");
+const search = document.getElementById("search");
+const scrollBtn = document.getElementById("scrollTopBtn");
+
+// Get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  showMovies(data.results);
+}
+
+function showMovies(movies) {
+  main.innerHTML = "";
+  movies.forEach((movie) => {
+    const { title, poster_path, vote_average, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+        <img src="${IMG_PATH + poster_path}" alt="${title}" />
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>Overview</h3>
+            <p>${overview}</p>
+        </div>
+    `;
+
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if (vote >= 8) return "green";
+  else if (vote >= 5) return "orange";
+  else return "red";
+}
+
+// Search movies
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm && searchTerm !== "") {
+    getMovies(SEARCH_URL + searchTerm);
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const scrollBtn = document.getElementById("scrollTopBtn");
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      scrollBtn.classList.add("show");
+    } else {
+      scrollBtn.classList.remove("show");
+    }
+  });
+
+  scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
